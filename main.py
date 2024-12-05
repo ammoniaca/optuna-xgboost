03b66403663cabe9ebf30305cc45641edeb287ec
Alessandro <alessandro.montaghi@gmail.com>
2024-12-05 10:21:12 +0100

    added more example in main

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ from optuna_xgb import Objective
 if __name__ == "__main__":
     X, y = make_regression(n_samples=200, n_features=1, n_targets=1, noise=30, random_state=0)
 
+    # https://optuna.readthedocs.io/en/stable/reference/pruners.html
+    # pruner = optuna.pruners.WilcoxonPruner(p_threshold=0.1
     pruner = optuna.pruners.MedianPruner(n_warmup_steps=5)
+
+    # https://optuna.readthedocs.io/en/stable/reference/samplers/index.html
+    # sampler = optuna.samplers.GPSampler()
+    # sampler = optuna.samplers.CmaEsSampler()
+    # sampler = optuna.samplers.NSGAIIISampler()
+    #
     sampler = optuna.samplers.TPESampler()
 
     study = optuna.create_study(pruner=pruner, sampler=sampler, direction="minimize")
     study.optimize(
-        Objective(X_train=X, y_train=y, cv_folds=5),
+        Objective(
+            X_train=X,
+            y_train=y,
+            cv_folds=5
+        ),
         n_trials=100,
         n_jobs=-1,
         show_progress_bar=True
@@ -22,6 +34,8 @@ if __name__ == "__main__":
     trial = study.best_trial
 
     print("  Value: {}".format(trial.value))
+
+    print(f"best value: {study.best_value}")
 
     print("  Params: ")
     for key, value in trial.params.items():
